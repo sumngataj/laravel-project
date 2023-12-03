52948b797ba2738933821e4f7105f36da01c9923
sumngataj <ajbeyyy@gmail.com>
2023-12-04 01:27:34 +0800

    chatbox pending

---
+++ resources/views/components/broadcast.blade.php
@@ -0,0 +1,9 @@
+<div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end message">
+                    <div>
+                        <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
+                            <p class="text-sm">{{$message}}
+                            </p>
+                        </div>
+                    </div>
+                    <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+</div>
