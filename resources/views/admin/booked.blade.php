8a34b33d6125940234459d1db184ac7c6e1df5cc
chanvapor <chanvapor1@gmail.com>
2024-03-31 23:48:29 +0800

    edit_venue

--- resources/views/admin/booked.blade.php
+++ resources/views/admin/booked.blade.php
@@ -105,7 +105,7 @@
                                                     </td>
                                                     <td class="px-4 py-3">
                                                         <div class="flex items-center text-sm">
-                                                            <p class="font-semibold">{{ $reservation->venue->name }}</p>
+                                                            <p class="font-semibold">{{ $reservation->venue_name }}</p>
                                                         </div>
                                                     </td>
                                                     <td class="px-4 py-3">
@@ -345,7 +345,7 @@
                                                     </td>
                                                     <td class="px-4 py-3">
                                                         <div class="flex items-center text-sm">
-                                                            <p class="font-semibold">{{ $reservation->venue->name }}</p>
+                                                            <p class="font-semibold">{{ $reservation->venue_name }}</p>
                                                         </div>
                                                     </td>
                                                     <td class="px-4 py-3">
--- resources/views/custombooking.blade.php
+++ resources/views/custombooking.blade.php
@@ -639,7 +639,7 @@
                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                     d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
                                             </svg>
-                                            {{$venue->capacity}}
+                                            Maximum of {{$venue->capacity}} persons
                                         </div>
                                     </div>
                                 </div>
@@ -687,6 +687,7 @@
                             </div>
                             <input type="hidden" name="user_id" id="user_id" value="{{ Auth::user()->id }}">
                             <input type="hidden" name="venue_id" id="venue_id" value="">
+                            <input type="hidden" name="venues_name" id="venues_name" value="">
                             <input type="hidden" name="guests" id="guestInput" value="">
                             <input type="hidden" name="price" id="price" value="">
                             <input type="hidden" name="add_ons" id="add_ons" value="">
@@ -1190,6 +1191,8 @@
                 updateTotalSummaryPrice(venuePrice);
                 const displayVenueName = document.getElementById('display-venue');
                 displayVenueName.innerHTML = venueName;
+                const venueNameInput = document.getElementById('venues_name');
+                venueNameInput.value = venueName;
                 // const displayCapacity = document.getElementById('display-capacity');
                 // displayCapacity.innerHTML = venueCapacity + "People";
                 const displayLocation = document.getElementById('display-venue-location');
